bf00d7e948ae6933289342e22fa0dd8d9ad3d557
David Joshua <davidjosh_87@yahoo.com>
2018-05-01 22:26:56 +0700

    first push master

--- app.js
+++ app.js
@@ -1,18 +1,16 @@
 
-const createError     = require('http-errors');
-const path            = require('path');
 const app             = require('express')();
 const bodyParser      = require('body-parser');
 const indexRoutes     = require('./routes');
 const providersRoutes = require('./routes/provider');
 const routeCustomer   = require('./routes/customer');
 const session         = require('express-session');
+const port            = process.env.PORT || '3000';
 
 //helpers
 app.locals.formatCurency = require('./helpers/formatCurency.js');
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // Body Parser
@@ -31,4 +29,4 @@ app.use('/provider', providersRoutes);
 app.use('/customer', routeCustomer);
 
 // -----------------------------------------------------------------------------
-module.exports = app;
+app.listen(port, console.log(`Listening on port ${port}`));
